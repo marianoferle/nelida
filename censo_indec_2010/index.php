d3d98bdd5f0475f48529ce6e72c99e5ab262bf2b
Mariano Ivan Ferle <marianoferle@gmail.com>
2022-02-27 11:26:03 -0300

    Delete censo_indec_2010 directory

--- censo_indec_2010/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once('includes/conexion.php'); ?>
-<?php include 'includes/header.php'; ?>
-
-<canvas id="canvass" data-processing-sources="processing/estadistica.pde">
-<p id="cann">No hay soporte para canvas</p>
-</canvas>  
-
-<?php include 'includes/footer.php'; ?>
-
